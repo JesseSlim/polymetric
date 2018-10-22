8ea69e745e79c159628c431e9a98d59c2ea16d6b
Jesse Slim <jesse.g.slim@gmail.com>
2018-10-22 10:43:39 +0200

    transformations added

--- polymetric/polymetric.py
+++ polymetric/polymetric.py
@@ -1,28 +1,45 @@
 import numpy as np
-import shapely as sp
+import shapely
 import shapely.geometry
 import shapely.ops
+import shapely.affinity
+import copy
 
 class Shape:
     DEFAULT_PARAMS = {
     }
-    def __init__(self, name=None, **kw):
-        self._params = {**Shape.DEFAULT_PARAMS, **kw}
+    def __init__(self, children=[], name=None, **kw):
+        # combine all default parameters from subclasses
+        # process bottom to top (so higher classes can override params)
+        subclasses = self.__class__.__mro__[::-1]
+        self._params = {}
+        for subcl in subclasses:
+            if hasattr(subcl, "DEFAULT_PARAMS"):
+                self._params.update(subcl.DEFAULT_PARAMS)
+        # finally add any user-supplied params
+        self._params.update(kw)
+
         self.name = name
-        self.children = []
+        self.children = children
         self.built = False
 
     def set_params(self, **kw):
+        self.built = False
         self._params.update(kw)
 
     def get_param(self, name, do_conversion=True):
         p = self._params[name]
         if do_conversion and callable(p):
-            return p(self._params)
+            return p(self.get_param)
         else:
             return p
 
-    def polygonize(self):
+    def polygonize(self, rebuild=False):
+        if not self.built or rebuild:
+            self.build()
+        return self._polygonize()
+
+    def _polygonize(self):
         raise NotImplementedError("Shapes must override the polygonize-method")
 
     def build(self):
@@ -31,64 +48,118 @@ class Shape:
     def is_built(self):
         return self.built
 
-class PositionedShape(Shape):
+    def clone(self):
+        return copy.deepcopy(self)
+
+    def apply(self, cls, name="", **kw):
+        return cls(children=[self], name=name, **kw)
+
+class Transformed(Shape):
+    DEFAULT_PARAMS = {
+        "transformer": lambda get_param, polygon: polygon,
+    }
+
+    def _polygonize(self):
+        transformer = self.get_param("transformer", do_conversion=False)
+
+        transformed_polys = []
+        for c in self.children:
+            c_poly = c.polygonize()
+            tc_poly = transformer(self.get_param, c_poly)
+
+            transformed_polys.append(tc_poly)
+
+        return transformed_polys
+
+class Scaled(Transformed):
+    DEFAULT_PARAMS = {
+        "scales": (1.0, 1.0),
+        "origin": (0.0, 0.0),
+        "transformer": lambda get_param, polygon: shapely.affinity.scale(
+            polygon, 
+            xfact=get_param("scales")[0],
+            yfact=get_param("scales")[0],
+            zfact=1.0, # we don't deal in 3D objects (yet)
+            origin=get_param("origin")
+            ),
+    }
+
+class Rotated(Transformed):
+    DEFAULT_PARAMS = {
+        "angle": 0.0,
+        "origin": (0.0, 0.0),
+        "transformer": lambda get_param, polygon: shapely.affinity.rotate(
+            polygon,
+            angle=get_param("angle"),
+            origin=get_param("origin"),
+            use_radians=True
+        )
+    }
+
+class Translated(Transformed):
+    DEFAULT_PARAMS = {
+        "offset": (0.0, 0.0),
+        "transformer": lambda get_param, polygon: shapely.affinity.translate(
+            polygon,
+            xoff=get_param("offset")[0],
+            yoff=get_param("offset")[1],
+            zoff=0.0, # we don't deal in 3D objects (yet)
+        )
+    }
+
+class Skewed(Transformed):
+    DEFAULT_PARAMS = {
+        "angles": (0.0, 0.0),
+        "origin": (0.0, 0.0),
+        "transformer": lambda get_param, polygon: shapely.affinity.skew(
+            polygon,
+            xs=get_param("angle")[0],
+            ys=get_param("angle")[1],
+            origin=get_param("origin"),
+            use_radians=True
+        )
+    }
+
+class Positioned(Shape):
     DEFAULT_PARAMS = {
         "x": 0.0,
         "y": 0.0,
     }
-    def __init__(self, name=None, **kw):
-        combined_params = {**PositionedShape.DEFAULT_PARAMS, **kw}
-        super().__init__(name=name, **combined_params)
 
-class Ellipse(PositionedShape):
+class Ellipse(Positioned):
     DEFAULT_PARAMS = {
         "a": 1.0,
         "b": 1.0,
         "n_sectors": 24,
+        "alpha_0": 0.0
     }
 
-    def __init__(self, name=None, **kw):
-        combined_params = {**Ellipse.DEFAULT_PARAMS, **kw}
-        super().__init__(name=name, **combined_params)
-
-    def polygonize(self):
-        self.build()
-
-        angle_list = np.linspace(0, 2*np.pi, self.get_param("n_sectors"), endpoint=False)
+    def _polygonize(self):
+        angle_list = np.linspace(0, 2*np.pi, self.get_param("n_sectors"), endpoint=False) + self.get_param("alpha_0")
 
         xs = self.get_param("x") + self.get_param("a") * np.cos(angle_list)
         ys = self.get_param("y") + self.get_param("b") * np.sin(angle_list)
 
         coords = zip(xs, ys)
 
-        polygon = sp.geometry.Polygon(coords)
+        polygon = shapely.geometry.Polygon(coords)
 
-        return polygon
+        return [polygon]
 
 class Circle(Ellipse):
     DEFAULT_PARAMS = {
         "r": 1.0,
-        "a": lambda params: params["r"],
-        "b": lambda params: params["r"],
+        "a": lambda get_param: get_param("r"),
+        "b": lambda get_param: get_param("r"),
     }
-    
-    def __init__(self, name=None, **kw):
-        combined_params = {**Circle.DEFAULT_PARAMS, **kw}
-        super().__init__(name=name, **combined_params)
 
-class Rectangle(PositionedShape):
+class Rectangle(Positioned):
     DEFAULT_PARAMS = {
         "w": 1.0,
         "h": 1.0,
     }
 
-    def __init__(self, name=None, **kw):
-        combined_params = {**Rectangle.DEFAULT_PARAMS, **kw}
-        super().__init__(name=name, **combined_params)
-
-    def polygonize(self):
-        self.build()
-
+    def _polygonize(self):
         width = np.abs(self.get_param("w"))
         height = np.abs(self.get_param("h"))
 
@@ -100,9 +171,9 @@ class Rectangle(PositionedShape):
 
         coords = zip(xs, ys)
 
-        polygon = sp.geometry.Polygon(coords)
+        polygon = shapely.geometry.Polygon(coords)
 
-        return polygon
+        return [polygon]
 
 class ParametricSweep(Shape):
     DEFAULT_PARAMS = {
@@ -111,10 +182,6 @@ class ParametricSweep(Shape):
         "sweep_params": {},
         "fixed_params": {}
     }
-
-    def __init__(self, name=None, **kw):
-        combined_params = {**ParametricSweep.DEFAULT_PARAMS, **kw}
-        super().__init__(name=name, **combined_params)
 
     def build(self):
         sweep_over = self.get_param("sweep_over")
@@ -141,13 +208,12 @@ class ParametricSweep(Shape):
         self.children = children
         self.built = True
     
-    def polygonize(self):
-        self.build()
+    def _polygonize(self):
         children_polys = []
 
         for child in self.children:
-            children_polys.append(child.polygonize())
+            children_polys += child.polygonize()
 
-        polygon = sp.ops.cascaded_union(children_polys)
+        # polygon = shapely.ops.cascaded_union(children_polys)
 
-        return polygon
+        return children_polys
