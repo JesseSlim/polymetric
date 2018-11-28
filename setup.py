bd17b42da2dd7321a7d720ba1cefd632f58d376a
Jesse Slim <jesse.g.slim@gmail.com>
2018-11-28 17:20:50 +0100

    version bump to 0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="polymetric",
-    version="0.2.0",
+    version="0.4.0",
     author="Jesse Slim",
     author_email="jesse.j.slim@gmail.com",
     description="Generate complex polygon structures",
