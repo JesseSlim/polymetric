36a119f9cbaf228db331373495735e12e7c4bff6
Jesse Slim <jesse.g.slim@gmail.com>
2020-03-04 08:38:30 +0100

    loading of gds files

--- polymetric/exporters/gds.py
+++ polymetric/exporters/gds.py
@@ -1,8 +1,10 @@
 import gdspy
 from importlib import reload
+from .. import polymetric as pm
 
 
-# should we reload the gdspy module every time something is saved?
+# should we reload the gdspy module every time something is saved/loaded?
+# GDSPY keeps some global library of stuff in memory that might need to be wiped
 DO_RELOAD = True
 
 # maximum number of vertices per polygon. Absolute limit set by GDS file format is 8191
@@ -24,8 +26,7 @@ def save(shape, filename, cell_name="POLYGON", datatype=1000,
 
     for vl in vertex_lists:
         if len(vl) > MAX_VERTICES:
-            raise ValueError("Polygon contains {} vertices, more than"
-                " the limit of {} vertices".format(len(vl), MAX_VERTICES))
+            raise ValueError(f"Polygon contains {len(vl)} vertices, more than the limit of {MAX_VERTICES} vertices")
 
         # shapely always duplicates the first vertex as the last vertex, get rid of that
         gds_poly = gdspy.Polygon(vl[:-1], datatype=datatype)
@@ -59,3 +60,22 @@ def save_multiple(shapes, filename, layers=None, cell_name="POLYGON", datatype=1
             poly_cell.add(gds_poly)
 
     gdspy.write_gds(filename, **kw)
+
+
+def load(filename):
+    if DO_RELOAD:
+        reload(gdspy)
+
+    gdslib = gdspy.GdsLibrary(infile=filename)
+
+    polys_per_layer = {}
+
+    for _, cell in gdslib.cell_dict.items():
+        for p1 in cell.polygons:
+            for (layer, poly) in zip(p1.layers, p1.polygons):
+                if layer not in polys_per_layer:
+                    polys_per_layer[layer] = []
+
+                polys_per_layer[layer].append(pm.Polygon(shell=poly))
+
+    return polys_per_layer
