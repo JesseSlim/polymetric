47e8e97584f0136a5bea78d81728aaba00fd27cb
Jesse Slim <jesse.g.slim@gmail.com>
2020-03-04 08:49:25 +0100

    little changes in plotting code

--- polymetric/plotting.py
+++ polymetric/plotting.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 import shapely as sp
 import shapely.geometry
 
+import copy
 
-def plot_polys(polys, cycle_colours=False, **kw):
+
+def plot_polys(polys, cycle_colours=False, interior_ls='--', **kw):
     single_color = None
     for i, outer_poly in enumerate(polys):
         if isinstance(outer_poly, (sp.geometry.MultiPolygon)):
@@ -15,20 +17,20 @@ def plot_polys(polys, cycle_colours=False, **kw):
         
         for poly in inner_poly:
             xs, ys = poly.exterior.xy
-            plot_params = dict(kw)
+            plot_params = copy.deepcopy(kw)
 
             if cycle_colours:
-                plot_params["color"] = "C%d" % (i % 10)
+                plot_params.setdefault("color", "C%d" % (i % 10))
             elif single_color is not None:
-                plot_params["color"] = single_color
+                plot_params.setdefault("color", single_color)
 
             l = plt.plot(xs, ys, **plot_params)
             if single_color is None:
                 single_color = l[0].get_color()
-                plot_params["color"] = single_color
+                plot_params.setdefault("color", single_color)
 
             # plot interiors with a different line style
-            plot_params["linestyle"] = "--"
+            plot_params.setdefault("linestyle", interior_ls)
             for interior in poly.interiors:
                 xs, ys = interior.xy
                 plt.plot(xs, ys, **plot_params)
